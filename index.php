d6720bff49af98f867382ca891345f4db9d76e63
Enes YEDIKARDES <54284965+eneeesyk@users.noreply.github.com>
2021-12-21 09:48:50 +0300

    merge index v1.1
    merged Hakan and enes index file

--- index.php
+++ index.php
@@ -93,28 +93,28 @@
             </div>
         </div>
         <div class='row'>
-        <div class='col'>
-                <div class='uni'><b>Şehirler:</b></div>
+        <div class='col ml-3'>
+                <div class=''><b>Şehirler:</b></div>
                 <div>
                 <?php
-                    $cities = array("Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara",
-                    "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl",
-                    "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce",
-                    "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", 
-                    "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", 
-                    "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya",
-                    "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize",
-                    "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop", "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", 
-                    "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak");
-
-                    echo '<div class="btn-group bootstrap-select show-tick ng-pristine ng-untouched ng-valid ng-empty">';
-                    echo "<select name='cities[]' class='selectpicker' multiple data-live-search='true'>";
-                    echo "<option selected disabled>Şehir Seçiniz.</option>";
-                    foreach($cities as $key => $value){
-                        echo '<option>'.$value.'</option>';
+                    $sql = "SELECT DISTINCT city FROM sayfa2";
+                    if ($result = mysqli_query($conn, $sql)) {
+                        if(mysqli_num_rows($result) > 0) {
+                            echo '<div class="btn-group bootstrap-select show-tick ng-pristine ng-untouched ng-valid ng-empty">';
+                            echo "<select name='cities[]' class='selectpicker' multiple data-live-search='true'>";
+                            echo "<option selected disabled>Şehir Seçiniz.</option>";
+                            while($row = mysqli_fetch_array($result)){    
+                                echo "<option>".$row['city']."</option>";
+                            }
+                            echo '</select>';
+                            echo '</div>';
+                            mysqli_free_result($result);
+                        }else {
+                            echo "<script type='text/JavaScript'> alert('Something went wrong...); </script>";
+                        }
+                    }else {
+                        echo "ERROR: Could not execute $sql" . mysqli_error($conn);
                     }
-                    echo '</div>';
-                    echo '</select>';
                     ?>
                 </div>
             </div>
